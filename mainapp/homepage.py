9c5780a4fb4467425229a52573be64fe4372fb3f
Code Warlock <dessymichaeltgp123@gmail.com>
2024-08-20 19:40:41 +0100

    More data setup

--- mainapp/homepage.py
+++ mainapp/homepage.py
@@ -3,54 +3,82 @@ data = {
     'headline2' : " & gambling offers",
     'sub_headline' : "Welcome to RyptocurrencyStakes, where innovation meets opportunity. Explore special cryptocurrency investments and top-tier gambling deals. Dive into the world of cryptocurrency and gambling with our expert-backed offers.",
     'what_we_offer' : "Discover Unmatched Offers Tailored to Your Needs",
+    'what_we_offer2' : "Discover Tailored Offers",
 
     # Hero Section
     'hero_cta_primary' : "Get Started Today",
     'hero_cta_secondary' : "Learn More",
 
-    # Exclusive Cryptocurrency Opportunities
-    'crypto_offers_title' : "Exclusive Cryptocurrency Opportunities",
-    'crypto_offers_description' : "Explore exclusive handpicked cryptocurrency investment platforms curated by industry experts to maximize your profit potential with precision and reliability.",
-    'crypto_offers_cta' : "Learn More",
+    'what_we_offer_list' : [
+        # Exclusive Cryptocurrency Opportunities
+        {
+            'title' : "Exclusive Cryptocurrency Opportunities",
+            'description' : "Explore exclusive handpicked cryptocurrency investment platforms curated by industry experts to maximize your profit potential with precision and reliability.",
+            'cta' : "Learn More"
+        },
 
     # Top-Tier Sports Betting and Casino Deals
-    'gambling_offers_title' : "Top-Tier Sports Betting and Casino Deals",
-    'gambling_offers_description' : "Experience the thrill of top-tier sports betting and casino gaming with the best odds and bonuses from trusted partners.",
-    'gambling_offers_cta' : "Start Betting",
+        {
+            'title' : "Top-Tier Sports Betting and Casino Deals",
+            'description' : "Experience the thrill of top-tier sports betting and casino gaming with the best odds and bonuses from trusted partners.",
+            'cta' : "Start Betting"    
+        },
 
     # Real-Time Market Insights and Data
-    'market_insights_title' : "Real-Time Market Insights and Data",
-    'market_insights_description' : "Stay ahead with real-time data and market insights tailored to your needs. Make informed decisions in both crypto trading and betting.",
-    'market_insights_cta' : "Get Insights",
+       {
+            'title' : "Real-Time Market Insights and Data",
+            'description' : "Stay ahead with real-time data and market insights tailored to your needs. Make informed decisions in both crypto trading and betting.",
+            'cta' : "Get Insights"   
+        },
 
     # User-Friendly Platform with 24/7 Support
-    'platform_support_title' : "User-Friendly Platform with 24/7 Support",
-    'platform_support_description' : "Navigate with confidence, whether you're betting or investing, with our intuitive platform and 24/7 dedicated support.",
-    'platform_support_cta' : "Join Now",
-
+        {
+            'title' : "User-Friendly Platform with 24/7 Support",
+            'description' : "Navigate with confidence, whether you're betting or investing, with our intuitive platform and 24/7 dedicated support.",
+            'cta' : "Join Now"    
+        },
+    ],
     # Featured Offers
     'featured_offers_title' : "Our Top Picks for You",
+    'crypto_offers' : [
+        {
+            'title' : "AI-Powered Crypto Trading with Avage AI",
+            'description' : "Leverage the power of AI to enhance your crypto trading strategies and maximize returns.",
+            'cta' : "Discover Avage AI",
+            'tab' : "1"
+        },
+         {
+            'title' : "Maximize Your Cryptocurrency Profits with BitQt",
+            'description' : "Join BitQt to explore advanced trading tools and achieve higher profitability.",
+            'cta' : "Learn About BitQt",
+            'tab' : "1"
+        },
+        {
+            'title' : "Join the Revolution of Digital Currencies",
+            'description' : "Be part of the future with Ethereum Code's cutting-edge cryptocurrency investments.",
+            'cta' : "Explore Ethereum Code",
+            'tab' : "1"
+        }
+    ],
+
     
-    'avage_ai_title' : "AI-Powered Crypto Trading with Avage AI",
-    'avage_ai_description' : "Leverage the power of AI to enhance your crypto trading strategies and maximize returns.",
-    'avage_ai_cta' : "Discover Avage AI",
-
-    'bitqt_title' : "Maximize Your Cryptocurrency Profits with BitQt",
-    'bitqt_description' : "Join BitQt to explore advanced trading tools and achieve higher profitability.",
-    'bitqt_cta' : "Learn About BitQt",
-
-    'ethereum_code_title' : "Join the Revolution of Digital Currencies",
-    'ethereum_code_description' : "Be part of the future with Ethereum Code's cutting-edge cryptocurrency investments.",
-    'ethereum_code_cta' : "Explore Ethereum Code",
 
     # Call to Action
     'cta_section_title' : "Elevate Your Game: Invest in Crypto, Bet with Confidence",
     'cta_primary_button' : "Sign Up Now",
     'cta_secondary_button' : "Explore Our Offers",
+    
+    # Mission
+    
+    'mission_taglines' : {
+    'empower': "Empower You to Win Big",
+    'invest': "Make Informed Investments",
+    'entertain': "Enjoy Thrilling Entertainment",
+    },
+    'mission' : "At RyptocurrencyStakes, Our mission is to bring you the finest opportunities in betting, casinos, and cryptocurrency investments.",
 
     # Footer - About Us
     'footer_about_title' : "About Us",
-    'footer_about_description' : "At RyptocurrencyStakes, we bring you the finest opportunities in betting, casinos, and cryptocurrency investments. Our mission is to empower you to win big, make informed investments, and enjoy a world of thrilling entertainment.",
     'footer_about_cta' : "Learn More About Us",
 
     # Footer - Quick Links

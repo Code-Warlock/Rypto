4910daba2a5e339b573133a53c9578234f69a90d
Killykinfolk <151724750+Killykinfolk@users.noreply.github.com>
2024-09-17 13:22:31 +0100

    Update homepage.py

--- mainapp/homepage.py
+++ mainapp/homepage.py
@@ -121,7 +121,7 @@ data = {
                   ],
                   "cta": "START WINNING AT JOYA CASINO",
                   'image' : 'assets/images/fortunejack_casino.jpeg',
-                  'url' : "https://offerone.g2afse.com/click?pid=444&offer_id=48",
+                  'url' : "https://taal.gotrackier.com/click?campaign_id=541&pub_id=710",
                   'tab' : "2" 
             },
             {
@@ -138,7 +138,7 @@ data = {
                   ],
                   "cta": "SIGN UP NOW AT NINEWIN CASINO",
                   'image' : 'assets/images/fortunejack_casino.jpeg',
-                  'url' : "https://offerone.g2afse.com/click?pid=444&offer_id=48",
+                  'url' : "https://taal.gotrackier.com/click?campaign_id=542&pub_id=710",
                   'tab' : "2" 
             }                   
 

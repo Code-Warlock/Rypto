d47672338143ab2809553e027dc607ecd90bf67b
Code Warlock <dessymichaeltgp123@gmail.com>
2024-09-25 07:32:12 +0100

    Noindex on offers2

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,13 +4,13 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("about/", views.about, name="about"),
-    path("crypto/", views.crypto, name="crypto"),
-    path("crypto/<slug:slug>/", views.crypto_offer, name="c_offer"),
-    path("gambling/", views.gambling, name="gambling"),
-    path("gambling/<slug:slug>/", views.gambling_offer, name="offer"),
-    path("contact/", views.contact, name="contact"),
-    path("privacy-policy/", views.privacy, name="privacy"),
-    path("cookie-policy/", views.cookie, name="cookie"),
+    path("about", views.about, name="about"),
+    path("crypto", views.crypto, name="crypto"),
+    path("crypto/<slug:slug>", views.crypto_offer, name="c_offer"),
+    path("gambling", views.gambling, name="gambling"),
+    path("gambling/<slug:slug>", views.gambling_offer, name="offer"),
+    path("contact", views.contact, name="contact"),
+    path("privacy-policy", views.privacy, name="privacy"),
+    path("cookie-policy", views.cookie, name="cookie"),
     path('robots.txt', views.robots_txt, name='robots_txt'),
 ]
